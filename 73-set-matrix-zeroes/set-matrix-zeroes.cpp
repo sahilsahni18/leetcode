66353c0df2a8979adfd97856ba8421cd141cd8a5
sahilsahni18 <150727382+sahilsahni18@users.noreply.github.com>
2024-12-07 18:42:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (10.75%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+   void setZeroes(vector<vector<int>>& matrix) {
     int m = matrix.size(), n = matrix[0].size();
-    vector<int> row(m, 0), col(n, 0);
-    
-    // First pass: mark rows and columns to be zeroed
+    bool firstRowZero = false, firstColZero = false;
+
+    // Check if the first row and first column need to be zeroed
+    for (int j = 0; j < n; ++j) {
+        if (matrix[0][j] == 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
     for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
+        if (matrix[i][0] == 0) {
+            firstColZero = true;
+            break;
+        }
+    }
+
+    // Use first row and column as markers
+    for (int i = 1; i < m; ++i) {
+        for (int j = 1; j < n; ++j) {
             if (matrix[i][j] == 0) {
-                row[i] = 1;
-                col[j] = 1;
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
             }
         }
     }
-    
-    // Second pass: zero out the rows and columns
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (row[i] == 1 || col[j] == 1) {
+
+    // Zero out cells based on markers
+    for (int i = 1; i < m; ++i) {
+        for (int j = 1; j < n; ++j) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                 matrix[i][j] = 0;
             }
         }
     }
+
+    // Zero out the first row and column if needed
+    if (firstRowZero) {
+        for (int j = 0; j < n; ++j) {
+            matrix[0][j] = 0;
+        }
     }
+    if (firstColZero) {
+        for (int i = 0; i < m; ++i) {
+            matrix[i][0] = 0;
+        }
+    }
+}
+    
 };
