4b948fce466a65e21c0ba2460709c234f23998f1
sahilsahni18 <150727382+sahilsahni18@users.noreply.github.com>
2025-01-04 14:29:54 +0530

    Time: 158 ms (71.47%) | Memory: 16.1 MB (47.85%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int n=s.size();
-        vector<int> start(26,-1),end(26,n); // first and last character index
-        for(int i=0;i<n;i++){
-            if(start[s[i]-'a']==-1) start[s[i]-'a']=i;
-            end[s[i]-'a']=i;
-        }
-        int ans=0;
-        for(int i=0;i<26;i++){
-            if(start[i]!=-1 && end[i]!=n){ // if character is not present
-                unordered_set<int> st;
-                for(int j=start[i]+1;j<end[i];j++) st.insert(s[j]);
-                ans+= st.size();
+        int ans = 0;
+        int n = s.length();
+        
+        unordered_map<char, pair<int, int>> M;
+        
+        for (int i = 0; i < n; i++) {
+            if (M.find(s[i]) == M.end()) {
+                M[s[i]].first = i;
             }
+            M[s[i]].second = i;
         }
+        
+        for (auto& [ch, positions] : M) {
+            int left = positions.first;
+            int right = positions.second;
+            
+            if (right - left <= 1) continue;  // No valid subsequences in this range
+            
+            unordered_set<char> uniqueChars;
+            for (int i = left + 1; i < right; i++) {
+                uniqueChars.insert(s[i]);
+            }
+            
+            ans += uniqueChars.size();  // Count the unique characters between first and last occurrence
+        }
+        
         return ans;
     }
 };
