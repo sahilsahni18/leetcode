09cbefb0fb48c2020d2b21bd84fc55ca892141a1
sahilsahni18 <150727382+sahilsahni18@users.noreply.github.com>
2025-06-10 04:39:06 +0530

    Time: 58 ms (15.88%) | Memory: 29.9 MB (19.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,14 +4,24 @@ public:
         unordered_map<string, vector<string>> ans;
 
         for (string& s : strs) {
-            string key = s;
-            sort(key.begin(), key.end());
+            array<int, 26> count = {0};
+
+            // Count frequency of each letter in the string
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+
+            string key;
+            for (int num : count) {
+                key += to_string(num) + "#";
+            }
+
             ans[key].push_back(s);
         }
 
         vector<vector<string>> result;
         for (auto& entry : ans) {
-            result.push_back(entry.second);
+            result.push_back(move(entry.second));
         }
 
         return result;        
