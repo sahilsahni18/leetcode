7b3762275ceec7f6808593fc4eb693d705c22398
sahilsahni18 <150727382+sahilsahni18@users.noreply.github.com>
2025-06-16 01:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (35.30%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int climbStairs(int n, unordered_map<int, int>& memo) {
+    int climbStairs(int n) {
         if (n == 0 || n == 1) {
             return 1;
         }
-        if (memo.find(n) == memo.end()) {
-            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
-        }
-        return memo[n];
-    }
 
-    int climbStairs(int n) {
-        unordered_map<int, int> memo;
-        return climbStairs(n, memo);
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
