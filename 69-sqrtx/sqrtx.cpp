62494aec3c24d0ba069f08c3b577d1b28d4bb91d
sahilsahni18 <150727382+sahilsahni18@users.noreply.github.com>
2025-06-17 01:02:30 +0530

    Time: 2 ms (21.25%) | Memory: 8.5 MB (50.88%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+     int mySqrt(int x) {
+        int i = 1;
+        int j = x;
+        int ans = 0;
+        while (i <=j){
+            int mid = i + (j-i)/2;
+            // upper bound
+            if (mid <= x/mid){
+                i = mid +1;
+                ans = mid;
+            }
+            else
+                j = mid-1;
+        }
+        
+        return ans;
+    }
+};
