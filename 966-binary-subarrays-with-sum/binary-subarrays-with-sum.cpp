7aea98882d083fc506af75e034b5aa8ec86af239
Sahil Sahney <150727382+sahilsahni18@users.noreply.github.com>
2026-02-01 11:52:24 +0530

    Time: 23 ms (18.87%) | Memory: 40.3 MB (7.41%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,17 @@
+/*Given a binary array nums and an integer goal, 
+return the number of non-empty subarrays with a sum goal.*/
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int count = 0;
+        int sum = 0;
+        unordered_map<int, int> mp;
+        mp.insert({0, 1});
+        for (int it : nums) {
+            sum += it; 
+            count += mp[sum - goal];
+            mp[sum]++; 
+        }
+        return count;
+    }
+};
